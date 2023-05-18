6d3308db323ff8fd658a468d3845cbf11e6f7f1a
Sergio Beppu <beppu.sergio@gmail.com>
2023-05-18 14:22:08 -0300

    funcionalidades GET /urls/:id implementadas

--- src/controllers/url.controller.js
+++ src/controllers/url.controller.js
@@ -1,16 +1,17 @@
 import { nanoid } from "nanoid"
 import {db} from "../database/database.connection.js"
 
+
 export async function urlSubmission (req, res) {
     const {url, userId} = res.locals
-    const shortenUrl = nanoid(8)
+    const shortUrl = nanoid(8)
 
     try {
-        await db.query(`INSERT INTO urls (url, "shortenUrl", "userId") VALUES ($1, $2, $3)`, [url, shortenUrl, userId])
+        await db.query(`INSERT INTO urls (url, "shortUrl", "userId") VALUES ($1, $2, $3)`, [url, shortUrl, userId])
         
         const body = {
             "id": userId,
-            "shortenUrl": shortenUrl
+            "shortUrl": shortUrl
         }
 
         res.status(201).send(body)
@@ -18,3 +19,21 @@ export async function urlSubmission (req, res) {
         console.log(err.message)
     }
 } 
+
+export async function getUrlById (req, res) {
+    const {id} = req.params
+
+    try {
+        const getUrl = await db.query(`SELECT * FROM urls WHERE id=$1`, [id])
+        if(getUrl.rowCount === 0) return res.sendStatus(404)
+        const body = {
+            id: id,
+            shortUrl: getUrl.rows[0].shortUrl,
+            url: getUrl.rows[0].url
+        }
+
+        res.status(200).send(body)
+    } catch (err) {
+        console.log(err.message)
+    }
+}
--- src/routes/url.route.js
+++ src/routes/url.route.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import { validateUrlBody } from "../middlewares/url.validate.js"
 import { validateSession } from "../middlewares/auth.validate.js"
-import { urlSubmission } from "../controllers/url.controller.js"
+import { getUrlById, urlSubmission } from "../controllers/url.controller.js"
 import { urlSchema } from "../schemas/url.schema.js"
 
 const urlRouter = Router()
 
 urlRouter.post("/urls/shorten", validateSession, validateUrlBody(urlSchema), urlSubmission)
+urlRouter.get("/urls/:id", getUrlById)
 
 export default urlRouter
